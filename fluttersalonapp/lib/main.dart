c3fd061b23829c1a114ddb552550446b5823c64b
sarah358 <njerinjuguna358@gmail.com>
2022-12-06 09:51:08 +0300

    register and login page

--- fluttersalonapp/lib/main.dart
+++ fluttersalonapp/lib/main.dart
@@ -1,74 +1,45 @@
+//region imports
+import 'package:fluttersalonapp/screens/BMSplashScreen.dart';
+import 'package:fluttersalonapp/store/AppStore.dart';
+import 'package:fluttersalonapp/utils/AppTheme.dart';
+import 'package:fluttersalonapp/utils/BMConstants.dart';
+import 'package:fluttersalonapp/utils/BMDataGenerator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:nb_utils/nb_utils.dart';
 
-void main() {
-  runApp(const MyApp());
+AppStore appStore = AppStore();
+
+int currentIndex = 0;
+
+void main() async {
+  //region Entry Point
+  WidgetsFlutterBinding.ensureInitialized();
+  await initialize(aLocaleLanguageList: languageList());
+
+  appStore.toggleDarkMode(value: getBoolAsync(isDarkModeOnPref));
+
+  defaultRadius = 10;
+  defaultToastGravityGlobal = ToastGravity.BOTTOM;
+
+  runApp(MyApp());
+  //endregion
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Observer(
+      builder: (_) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: '$appName${!isMobile ? ' ${platformName()}' : ''}',
+        home: const BMSplashScreen(),
+        theme: !appStore.isDarkModeOn ? AppThemeData.lightTheme : AppThemeData.darkTheme,
+        navigatorKey: navigatorKey,
+        scrollBehavior: SBehavior(),
+        supportedLocales: LanguageDataModel.languageLocales(),
+        localeResolutionCallback: (locale, supportedLocales) => locale,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold( 
-          
-        body: Container(
-           
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/images/model_one.jpg'), fit: BoxFit.cover),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(16),
-              width: MediaQuery.of(context).size.width,
-              child: ElevatedButton.icon(  
-             onPressed: () {},
-                icon:Icon(Icons.phone),
-                label: const Text('LOGIN WITH PHONE'),
-                style: ButtonStyle(
-                  backgroundColor:MaterialStateProperty.all(Color.fromARGB(255, 101, 35, 180)) ), ),
-            )
-       
-          ],
-        ),
-      ),
-       
     );
   }
 }
--- fluttersalonapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ fluttersalonapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,18 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_auth
-import firebase_core
+import connectivity_plus_macos
+import nb_utils
+import package_info
 import path_provider_macos
+import shared_preferences_macos
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  NbUtilsPlugin.register(with: registry.registrar(forPlugin: "NbUtilsPlugin"))
+  FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
